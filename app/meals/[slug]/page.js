03d7f46ca30b7de833f44d94579a50c5433b4590
Dana Sharon <whosedreamisthis@gmail.com>
2024-05-31 11:27:59 -0700

    meal detail page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,29 @@
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
 export default function MealPage({ params }) {
+  const meal = getMeal(params.slug);
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
   return (
     <>
-      <header></header>
-      <main></main>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            By <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
     </>
   );
 }
