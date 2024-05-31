4fce392bdbb53691d4dd43cb1af84214bf5f252e
Dana Sharon <whosedreamisthis@gmail.com>
2024-05-31 13:01:59 -0700

    share page

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,5 +1,7 @@
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker.js';
+import { shareMeal } from '@/lib/actions.js';
+
 export default function ShareMealPage() {
   return (
     <>
@@ -10,7 +12,8 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        {/* action={shareMeal} */}
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -38,7 +41,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker label="Mario"></ImagePicker>
+          <ImagePicker label="Your image" name="image"></ImagePicker>
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
