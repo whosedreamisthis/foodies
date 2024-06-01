6b13c89fea1af287f0a6670bd4620f986252f20c
Dana Sharon <whosedreamisthis@gmail.com>
2024-05-31 17:01:11 -0700

    useFormState

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,13 @@
+'use client';
+import { useFormState } from 'react-dom';
 import classes from './page.module.css';
 import ImagePicker from '@/components/meals/image-picker.js';
 import { shareMeal } from '@/lib/actions.js';
 import MealsFormSubmit from '@/components/meals/meals-form-submit.js';
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, {
+    message: null,
+  });
   return (
     <>
       <header className={classes.header}>
@@ -13,7 +18,7 @@ export default function ShareMealPage() {
       </header>
       <main className={classes.main}>
         {/* action={shareMeal} */}
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,6 +47,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image"></ImagePicker>
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
--- components/meals/meals-form-submit.js
+++ components/meals/meals-form-submit.js
@@ -3,8 +3,6 @@ import classes from './meals-form-submit.module.css';
 import { useFormStatus } from 'react-dom';
 export default function MealsFormSubmit() {
   const { pending } = useFormStatus();
-  console.log('why is thsi ha');
-
   return (
     // <h1>hello</h1>
     <button className={classes.button} disabled={pending}>
